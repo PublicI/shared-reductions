d978e760b40ee6d1370827d2bc338f7c6fb23dcc
Chris Zubak-Skees <chriszs@gmail.com>
2017-04-04 19:05:58 -0400

    Tweaks

--- src/script/view/list.js
+++ src/script/view/list.js
@@ -1,27 +1,29 @@
 import template from '../../view/list.html';
 // import listModel from '../model/list';
 // import listController from '../controller/list';
-import {intword} from 'journalize';
 import {sheets} from '../../data/sheets.json';
+import {intword} from 'journalize';
+import smartquotes from 'smartquotes';
 
 //listModel.actions = listController;
 
 export default {
-	data() {
-		return {
-			list: sheets.shared_reductions
-		};
-	},
+    data() {
+        return {
+            list: sheets.shared_reductions
+        };
+    },
     // vuex: listModel,
     render: template.render,
     staticRenderFns: template.staticRenderFns,
     created() {
-    	console.log(sheets.shared_reductions);
-    	// this.load();
+        console.log(sheets.shared_reductions);
+        // this.load();
     },
-    methods: {
-    	moneyFormat(n) {
-    		return '$' + intword(parseInt(n.replace(/,/g,'').replace('$','')));
-    	}
+    filters: {
+        smartQuotes: smartquotes,
+        moneyFormat(n) {
+            return '$' + intword(parseInt(n.replace(/,/g,'').replace('$','')));
+        }
     }
 };
